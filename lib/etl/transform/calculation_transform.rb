1f56996ae7634a093ff1f2d8940d3ab518540f67
Fabien Carrion <fabien.carrion@gmail.com>
2011-05-31 13:32:40 -0500

    Add much more operations to calculation step

--- lib/etl/transform/calculation_transform.rb
+++ lib/etl/transform/calculation_transform.rb
@@ -15,15 +15,52 @@ module ETL
         return nil if row[@fields[0]].nil?
 
         if (@function.eql? "A + B")
-          first = row[@fields[0]]
-          second = ""
-          second = row[@fields[1]] unless row[@fields[1]].nil?
-          row[name] = first + second
+          result = ""
+          @fields.each do |field|
+            next if field.nil?
+
+            string = ""
+            if field.to_s.eql? field
+              string = field
+              begin
+                string = eval('"' + field + '"')
+              rescue
+              end
+            else
+              string = row[field]
+            end
+            next if string.nil?
+
+            result = result + string
+          end
+
+          row[name] = result
         end
 
         if (@function.eql? "date A")
           first = row[@fields[0]]
           row[name] = Time.parse(first)
+        end
+
+        if (@function.eql? "trim A")
+          first = row[@fields[0]]
+          row[name] = first.strip
+        end
+
+        if (@function.eql? "lower A")
+          first = row[@fields[0]]
+          row[name] = first.downcase
+        end
+
+        if (@function.eql? "upper A")
+          first = row[@fields[0]]
+          row[name] = first.upcase
+        end
+
+        if (@function.eql? "encoding A")
+          # Bug from ruby 1.8 http://po-ru.com/diary/fixing-invalid-utf-8-in-ruby-revisited/
+          first = row[@fields[0]]
+          row[name] = Iconv.conv(@fields[1], @fields[2], first + ' ')[0..-2]
         end
 
         row[name]
