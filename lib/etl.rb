3e83f6899a516430ee7e394737614f349a6df160
Thibaut Barrère <thibaut.barrere@gmail.com>
2011-06-20 03:34:53 +0200

    Implement optional_require to allow to attempt loading of gems.

--- lib/etl.rb
+++ lib/etl.rb
@@ -49,6 +49,17 @@ else
   require 'csv'
 end
 
+# patch for https://github.com/activewarehouse/activewarehouse-etl/issues/24
+# allow components to require optional gems
+class Object
+  def optional_require(feature)
+    begin
+      require feature
+    rescue LoadError
+    end
+  end
+end
+
 $:.unshift(File.dirname(__FILE__))
 
 require 'etl/core_ext'
