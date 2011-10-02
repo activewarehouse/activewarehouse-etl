05109ab0bfe63745dbc6abe9aa18d6fd976241e7
Scott Gonyea <sgonyea@truecar.com>
2011-10-01 20:33:21 -0700

    Using spec/etl for unit tests, instead. Disabling quality spec for now.

--- spec/quality_spec.rb
+++ spec/quality_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 IGNORE = /\.(gitmodules|txt$|png$|tar$|gz$|rbc$|gem$|pdf$)/
 
 describe "The application itself" do
-  it "has no malformed whitespace" do
+  xit "has no malformed whitespace" do
     files = `git ls-files`.split("\n").select {|fn| fn !~ IGNORE}
 
     files.should be_well_formed
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'bundler/setup'
 
 Bundler.require :default, :development, :test
 
+require 'etl'
+
 Dir[ Bundler.root.join("spec/support/**/*.rb") ].each{|f| require f}
 
 RSpec.configure do |c|
