193f6200cde969c9fb0519de004418b8d74827f0
Scott Gonyea <sgonyea@truecar.com>
2011-10-01 18:35:26 -0700

    Copyright

--- lib/etl.rb
+++ lib/etl.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2006-2007 Anthony Eden
+# Copyright (c) 2006-2011 Anthony Eden
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
