16d05afa8461c4be0e501356ef90dfa594bf0a6c
Steve Meyfroidt <steve.meyfroidt@gmail.com>
2011-10-03 11:00:54 +0100

    Option for CSV destination to write header line

--- lib/etl/control/destination/csv_destination.rb
+++ lib/etl/control/destination/csv_destination.rb
@@ -49,6 +49,7 @@ module ETL #:nodoc:
         @unique = configuration[:unique] ? configuration[:unique] + scd_required_fields : configuration[:unique]
         @unique.uniq! unless @unique.nil?
         @order = mapping[:order] ? mapping[:order] + scd_required_fields : order_from_source
+        @write_header = configuration[:write_header]
         @order.uniq! unless @order.nil?
         raise ControlError, "Order required in mapping" unless @order
       end
@@ -63,6 +64,11 @@ module ETL #:nodoc:
       # Flush the destination buffer
       def flush
         #puts "Flushing buffer (#{file}) with #{buffer.length} rows"
+        if @write_header && !@header_written
+          f << order
+          @header_written = true
+        end
+
         buffer.flatten.each do |row|
           #puts "row change type: #{row.change_type}"
           # check to see if this row's compound key constraint already exists
