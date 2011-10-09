0e75acc1a444d530f48c30330684424d223eaa28
Scott Gonyea <sgonyea@truecar.com>
2011-10-08 19:36:43 -0700

    Tuned my custom_fixtures boilerplate for the etl project :).

--- spec/support/custom_fixtures.rb
+++ spec/support/custom_fixtures.rb
@@ -6,19 +6,21 @@ module CustomFixtures
   # @param [#to_sym]
   # @return [String]
   def fixture_for(name)
-    fixtures[name.to_sym][:fixture]
+    file_fixtures[name][:fixture]
   end
   alias :fixture :fixture_for
 
   # @param [#to_sym]
   # @return [String]
   def path_for(name)
-    fixtures[name.to_sym][:path]
+    file_fixtures[name][:path]
   end
+  alias :path         :path_for
+  alias :fixture_path :path_for
 
   # @see ConfigFixtures#create_fixture_hash
-  def fixtures
-    @fixtures ||= begin
+  def file_fixtures
+    @file_fixtures ||= begin
       hash = Hash.new {|k,v| k[v] = {}}
       hash.merge!(create_fixture_hash)
     end
@@ -32,19 +34,24 @@ module CustomFixtures
   # @param [String]
   # @return [Array<Symbol, String>]
   def map_fixture(fpath)
-    [symbolize_filename(fpath), {:path => fpath, :fixture => read_file(fpath)}]
+    [relative_path(fpath), {:path => fpath, :fixture => read_file(fpath)}]
   end
 
   # @return [Array<String>]
   def find_fixtures
-    Dir.glob Bundler.root.join('spec/fixtures/**/*')
+    @find_fixtures ||= begin
+      files = Dir[ fixture_root.join('**/*') ]
+      files.reject! { |fi| File.directory?(fi) }
+      files
+    end
   end
 
-  # @param [String]
-  # @return [Symbol]
-  def symbolize_filename(fpath)
-    fname = File.basename(fpath)
-    fname.split(/\W/).shift.to_sym
+  def fixture_root
+    @project_root ||= Bundler.root.join('spec/fixtures/')
+  end
+
+  def relative_path(from_path)
+    from_path.gsub(fixture_root.to_s, '')
   end
 
   # @param [String]
