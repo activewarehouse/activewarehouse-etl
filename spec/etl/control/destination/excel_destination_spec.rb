2ad9920cb6d813fb36eafbe745ea24488cb376db
Scott Gonyea <sgonyea@truecar.com>
2011-10-15 19:11:20 -0700

    Migrated the destination_test to RSpec, with the exception of the database tests.

---
+++ spec/etl/control/destination/excel_destination_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe ETL::Control::ExcelDestination do
+  let(:ctl_file){ fixture_path 'delimited_excel.ctl' }
+  let(:control) { ETL::Control.parse(ctl_file) }
+  let(:mapping) do
+    country_codes = { 'United States' => 'US', 'Mexico' => 'MX' }
+    cc_proc = proc {|r| country_codes[r[:country]]}
+
+    {:order => [:address, :city, :state, :country, :country_code], :virtual => { :country_code => cc_proc }}
+  end
+
+  let(:configuration) { {:file => outfile, :buffer_size => 0} }
+
+  let(:outfile) { fixture_root 'output/test_excel_destination.xls' }
+  let(:output) { File.read(outfile) }
+
+  let(:destination) { ETL::Control::ExcelDestination.new(control, configuration, mapping) }
+
+  let(:row) { ETL::Row[ :address => '123 SW 1st Street', :city => 'Melbourne', :state => 'Florida', :country => 'United States' ] }
+
+  before(:each) {
+    destination.write(row)
+    destination.close
+  }
+
+  context "When using the destination's defaults" do
+    let(:book) { Spreadsheet.open(outfile) }
+    let(:sheet) { book.worksheet(0) }
+
+    it "should output a basic Excel document" do
+      sheet[0, 0].should == "123 SW 1st Street"
+      sheet[0, 1].should == "Melbourne"
+      sheet[0, 2].should == "Florida"
+      sheet[0, 3].should == "United States"
+      sheet[0, 4].should == "US"
+    end
+  end
+end
---
+++ spec/etl/control/destination/file_destination_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe ETL::Control::FileDestination do
+  let(:ctl_file){ fixture_path 'delimited.ctl' }
+  let(:control) { ETL::Control.parse(ctl_file) }
+  let(:mapping) do
+    country_codes = { 'United States' => 'US', 'Mexico' => 'MX' }
+    cc_proc = proc {|r| country_codes[r[:country]]}
+
+    {:order => [:address, :city, :state, :country, :country_code], :virtual => { :country_code => cc_proc }}
+  end
+
+  let(:outfile) { fixture_root 'output/test_file_destination.txt' }
+  let(:output) { File.read(outfile) }
+
+  let(:destination) { ETL::Control::FileDestination.new(control, configuration, mapping) }
+
+  let(:simple_row) { ETL::Row[ :address => '123 SW 1st Street', :city => 'Melbourne', :state => 'Florida', :country => 'United States' ] }
+  let(:row_needs_escape) { ETL::Row[ :address => "Allen's Way", :city => 'Some City', :state => 'Some State', :country => 'Mexico' ] }
+
+  before(:each) {
+    destination.write(simple_row)
+    destination.write(row_needs_escape)
+    destination.close
+  }
+
+  context "When using the destination's defaults" do
+    let(:configuration) { {:file => outfile, :buffer_size => 0} }
+
+    it "should output a standard CSV style document" do
+      output.should == <<-DOCUMENT
+123 SW 1st Street,Melbourne,Florida,United States,US
+Allen's Way,Some City,Some State,Mexico,MX
+DOCUMENT
+    end
+  end
+
+  context "When using custom options for the document's output" do
+    let(:configuration) { {:file => outfile, :buffer_size => 0, :separator => '|', :eol => "[EOL]\n"} }
+
+    it "should use the custom separator / eol character(s)" do
+      output.should == <<-DOCUMENT
+123 SW 1st Street|Melbourne|Florida|United States|US[EOL]
+Allen's Way|Some City|Some State|Mexico|MX[EOL]
+DOCUMENT
+    end
+  end
+end
