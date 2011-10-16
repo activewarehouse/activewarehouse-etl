db98cde46ff702dc5e323972dd92590d9771c780
Scott Gonyea <sgonyea@truecar.com>
2011-10-15 21:31:58 -0700

    Minor verbiage changes.

--- spec/etl/control/destination_spec.rb
+++ spec/etl/control/destination_spec.rb
@@ -15,7 +15,7 @@ describe ETL::Control::Destination do
   let(:row2) { ETL::Row[:id => 2, :first_name => 'Bob', :last_name => 'Smith', :ssn => '111234444']  }
   let(:row3) { ETL::Row[:id => 3, :first_name => 'John', :last_name => 'Smith', :ssn => '000112222'] }
 
-  context "when the destinations are valid" do
+  context "When the records should be unique across field(s)" do
     before(:each) {
       destination.write(row1)
       destination.write(row2)
@@ -23,7 +23,7 @@ describe ETL::Control::Destination do
       destination.close
     }
 
-    context "When outputing only the records that are unique to a single column" do
+    context "When outputing only the records that are unique to a single field" do
       let(:configuration) { {:file => outfile, :buffer_size => 0, :unique => [:ssn]} }
 
       it "should exclude rows with a duplicate value, for the given column" do
@@ -34,7 +34,7 @@ DOCUMENT
       end
     end
 
-    context "When outputing only the records that are unique across multiple columns" do
+    context "When outputing only the records that are unique across multiple fields" do
       let(:row3) { ETL::Row[:id => 3, :first_name => 'Bob', :last_name => 'Smith', :ssn => '000112222'] }
       let(:configuration) { {:file => outfile, :buffer_size => 0, :unique => [:last_name,:first_name]} }
 
