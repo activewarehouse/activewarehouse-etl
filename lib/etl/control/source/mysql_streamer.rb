37a45c1e7fe8d26895b5516089bbf437f0133a23
Philip Dodds <philip.dodds@me.com>
2011-12-13 15:11:45 -0500

    Adding in the host

--- lib/etl/control/source/mysql_streamer.rb
+++ lib/etl/control/source/mysql_streamer.rb
@@ -11,7 +11,7 @@ class MySqlStreamer
 		puts "Using the Streaming MySQL from the command line"
 		keys = nil
 		connection_configuration = ETL::Base.configurations[@name.to_s]
-		mysql_command = """mysql --quick -u #{connection_configuration["username"]} -e \"#{@query.gsub("\n","")}\" -D #{connection_configuration["database"]} --password=#{connection_configuration["password"]} -B"""
+		mysql_command = """mysql --quick -h #{connection_configuration["host"]} -u #{connection_configuration["username"]} -e \"#{@query.gsub("\n","")}\" -D #{connection_configuration["database"]} --password=#{connection_configuration["password"]} -B"""
 		Open3.popen3(mysql_command) do |stdin, out, err, external|
 			until (line = out.gets).nil? do
 				line = line.gsub("\n","")
