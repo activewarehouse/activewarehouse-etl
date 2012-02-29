03c1983fbe52ba0e4ea49e8f93d0179941b4f097
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-02-29 21:04:12 +0100

    Clean up our test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,3 @@
-require 'rubygems'
-require 'bundler/setup'
-
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 $:.unshift(File.dirname(__FILE__))
 
@@ -10,18 +7,14 @@ require 'etl'
 require 'shoulda'
 require 'flexmock/test_unit'
 
+raise "Missing required DB environment variable" unless ENV['DB']
+
 database_yml = File.dirname(__FILE__) + '/config/database.yml'
 ETL::Engine.init(:config => database_yml)
 ETL::Engine.logger = Logger.new(STDOUT)
 # ETL::Engine.logger.level = Logger::DEBUG
 ETL::Engine.logger.level = Logger::FATAL
 
-db = YAML::load(IO.read(database_yml))['operational_database']['adapter']
-# allow both mysql2 and mysql adapters
-db = db.gsub('mysql2', 'mysql')
-raise "Unsupported test db '#{db}'" unless ['mysql', 'postgresql'].include?(db)
-
-require "connection/#{db}/connection"
 ActiveRecord::Base.establish_connection :operational_database
 ETL::Execution::Job.delete_all
 
