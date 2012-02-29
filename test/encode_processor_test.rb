d0615e88c2678474e0f817a97c5198e146876db6
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-02-29 21:02:20 +0100

    Add missing require

--- test/encode_processor_test.rb
+++ test/encode_processor_test.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 require File.dirname(__FILE__) + '/test_helper'
 
+require 'iconv'
+
 class EncodeProcessorTest < Test::Unit::TestCase
 
   SOURCE = 'data/encode_source_latin1.txt'
