5239c9b1828e23ce9c63748a35c424c4d2fe221b
Sinisa Grgic <sinisa@lumoslabs.com>
2012-03-06 12:43:03 +0100

    mysqlstream bug fix when store_locally is enabled

--- lib/etl/control/source/database_source.rb
+++ lib/etl/control/source/database_source.rb
@@ -4,13 +4,13 @@ module ETL #:nodoc:
   class Source < ::ActiveRecord::Base #:nodoc:
     # Connection for database sources
   end
-  
+
   module Control #:nodoc:
     # Source object which extracts data from a database using ActiveRecord.
     class DatabaseSource < Source
       attr_accessor :target
       attr_accessor :table
-      
+
       # Initialize the source.
       #
       # Arguments:
@@ -22,65 +22,72 @@ module ETL #:nodoc:
       # * <tt>:target</tt>: The target connection
       # * <tt>:table</tt>: The source table name
       # * <tt>:database</tt>: The database name
-      # 
+      #
       # Other options:
-      # * <tt>:join</tt>: Optional join part for the query (ignored unless 
+      # * <tt>:join</tt>: Optional join part for the query (ignored unless
       #   specified)
-      # * <tt>:select</tt>: Optional select part for the query (defaults to 
+      # * <tt>:select</tt>: Optional select part for the query (defaults to
       #   '*')
-      # * <tt>:group</tt>: Optional group by part for the query (ignored 
+      # * <tt>:group</tt>: Optional group by part for the query (ignored
       #   unless specified)
-      # * <tt>:order</tt>: Optional order part for the query (ignored unless 
+      # * <tt>:order</tt>: Optional order part for the query (ignored unless
       #   specified)
       # * <tt>:new_records_only</tt>: Specify the column to use when comparing
       #   timestamps against the last successful ETL job execution for the
       #   current control file.
-      # * <tt>:store_locally</tt>: Set to false to not store a copy of the 
+      # * <tt>:store_locally</tt>: Set to false to not store a copy of the
       #   source data locally in a flat file (defaults to true)
       def initialize(control, configuration, definition)
         super
         @target = configuration[:target]
         @table = configuration[:table]
         @query = configuration[:query]
+        if mysqlstream
+          @mysql_stream = MySqlStreamer.new(query,@target)
+        end
       end
-      
+
       # Get a String identifier for the source
       def to_s
         "#{host}/#{database}/#{@table}"
       end
-      
-      # Get the local directory to use, which is a combination of the 
+
+      # Get the local directory to use, which is a combination of the
       # local_base, the db hostname the db database name and the db table.
       def local_directory
         File.join(local_base, to_s)
       end
-      
+
       # Get the join part of the query, defaults to nil
       def join
         configuration[:join]
       end
-      
+
       # Get the select part of the query, defaults to '*'
       def select
         configuration[:select] || '*'
       end
-      
+
       # Get the group by part of the query, defaults to nil
       def group
         configuration[:group]
       end
-      
+
       # Get the order for the query, defaults to nil
       def order
         configuration[:order]
       end
-      
+
       # Return the column which is used for in the where clause to identify
       # new rows
       def new_records_only
         configuration[:new_records_only]
       end
-      
+
+      def mysqlstream
+        configuration[:mysqlstream]
+      end
+
       # Get the number of rows in the source
       def count(use_cache=true)
         return @count if @count && use_cache
@@ -90,16 +97,16 @@ module ETL #:nodoc:
           @count = connection.select_value(query.gsub(/SELECT .* FROM/, 'SELECT count(1) FROM'))
         end
       end
-      
+
       # Get the list of columns to read. This is defined in the source
       # definition as either an Array or Hash
       def columns
         # weird default is required for writing to cache correctly
         @columns ||= query_rows.any? ? query_rows.first.keys : ['']
       end
-      
+
       # Returns each row from the source. If read_locally is specified then
-      # this method will attempt to read from the last stored local file. 
+      # this method will attempt to read from the last stored local file.
       # If no locally stored file exists or if the trigger file for the last
       # locally stored file does not exist then this method will raise an
       # error.
@@ -124,13 +131,13 @@ module ETL #:nodoc:
           end
         end
       end
-      
+
       private
       # Read rows from the local cache
       def read_rows(file)
         raise "Local cache file not found" unless File.exists?(file)
         raise "Local cache trigger file not found" unless File.exists?(local_file_trigger(file))
-        
+
         t = Benchmark.realtime do
           CSV.open(file, :headers => true).each do |row|
             result_row = ETL::Row.new
@@ -143,13 +150,13 @@ module ETL #:nodoc:
         end
         ETL::Engine.average_rows_per_second = ETL::Engine.rows_read / t
       end
-      
+
       def count_locally
         counter = 0
         File.open(last_local_file, 'r').each { |line| counter += 1 }
         counter
       end
-      
+
       # Write rows to the local cache
       def write_local(file)
         lines = 0
@@ -165,28 +172,28 @@ module ETL #:nodoc:
         end
         ETL::Engine.logger.info "Stored locally in #{t}s (avg: #{lines/t} lines/sec)"
       end
-      
+
       # Get the query to use
       def query
         return @query if @query
         q = "SELECT #{select} FROM #{@table}"
         q << " #{join}" if join
-        
+
         conditions = []
         if new_records_only
-          last_completed = ETL::Execution::Job.maximum('created_at', 
+          last_completed = ETL::Execution::Job.maximum('created_at',
             :conditions => ['control_file = ? and completed_at is not null', control.file]
           )
           if last_completed
             conditions << "#{new_records_only} > #{connection.quote(last_completed.to_s(:db))}"
           end
         end
-        
+
         conditions << configuration[:conditions] if configuration[:conditions]
         if conditions.length > 0
           q << " WHERE #{conditions.join(' AND ')}"
         end
-        
+
         q << " GROUP BY #{group}" if group
         q << " ORDER BY #{order}" if order
 
@@ -196,31 +203,31 @@ module ETL #:nodoc:
           options[:offset] = ETL::Engine.offset if ETL::Engine.offset
           connection.add_limit_offset!(q, options)
         end
-        
+
         q = q.gsub(/\n/,' ')
         ETL::Engine.logger.info "Query: #{q}"
         @query = q
       end
-      
+
       def query_rows
         return @query_rows if @query_rows
-        if (configuration[:mysqlstream] == true)
-          MySqlStreamer.new(query,@target)
+        if mysqlstream
+          @mysql_stream.run_query
         else
           connection.select_all(query)
         end
       end
-      
+
       # Get the database connection to use
       def connection
         ETL::Engine.connection(target)
       end
-      
+
       # Get the host, defaults to 'localhost'
       def host
         ETL::Base.configurations[target.to_s]['host'] || 'localhost'
       end
-      
+
       def database
         ETL::Base.configurations[target.to_s]['database']
       end
--- lib/etl/control/source/mysql_streamer.rb
+++ lib/etl/control/source/mysql_streamer.rb
@@ -2,30 +2,32 @@ require 'open3'
 
 class MySqlStreamer
 
-	def initialize(query, target)
-		@query = query
-		@name = target
-	end
+  def initialize(query, target)
+    @query = query
+    @name = target
+  end
 
-	def each
-		puts "Using the Streaming MySQL from the command line"
-		keys = nil
-		connection_configuration = ETL::Base.configurations[@name.to_s]
-		mysql_command = """mysql --quick -h #{connection_configuration["host"]} -u #{connection_configuration["username"]} -e \"#{@query.gsub("\n","")}\" -D #{connection_configuration["database"]} --password=#{connection_configuration["password"]} -B"""
-		Open3.popen3(mysql_command) do |stdin, out, err, external|
-			until (line = out.gets).nil? do
-				line = line.gsub("\n","")
-				if keys.nil?
-					keys = line.split("\t")
-				else
-					hash = Hash[keys.zip(line.split("\t"))]
-					yield hash
-				end
-			end
-			error = err.gets
-			if (!error.nil? && error.strip.length > 0)
-				throw error
-			end
-		end
-	end
+  def run_query
+    puts "Using the Streaming MySQL from the command line"
+    query_result = []
+    keys = nil
+    connection_configuration = ETL::Base.configurations[@name.to_s]
+    mysql_command = """mysql --quick -h #{connection_configuration["host"]} -u #{connection_configuration["username"]} -e \"#{@query.gsub("\n","")}\" -D #{connection_configuration["database"]} --password=#{connection_configuration["password"]} -B"""
+    Open3.popen3(mysql_command) do |stdin, out, err, external|
+      until (line = out.gets).nil? do
+        line = line.gsub("\n","")
+        if keys.nil?
+          keys = line.split("\t")
+        else
+          hash = Hash[keys.zip(line.split("\t"))]
+          query_result << hash
+        end
+      end
+      error = err.gets
+      if error && error.strip.length > 0
+        throw error
+      end
+    end
+    query_result
+  end
 end
